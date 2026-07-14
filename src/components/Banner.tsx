2572cd4e1fd6a25eeb6d806edb8e4ee796471bad
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-14 14:38:22 +0330

    feat: Add Poppins Font

---
+++ src/components/Banner.tsx
@@ -0,0 +1,5 @@
+export default function Banner() {
+  return <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat impedit quidem quod id consequatur ex repudiandae itaque, quas obcaecati perferendis temporibus odit quaerat placeat aperiam voluptate? Aperiam molestias possimus, vel, explicabo dolorum quibusdam velit repellendus blanditiis a vitae ut labore veniam voluptatem architecto ea sint quod omnis, earum officia recusandae nostrum odio tempore! Ducimus inventore quod sapiente, recusandae nemo animi quae eos sint. Magni corrupti voluptatum excepturi, nam aspernatur modi ea alias voluptas vero nobis. Accusantium laboriosam cumque consequatur voluptas repellendus praesentium consequuntur eius, quos fugiat distinctio earum saepe dignissimos soluta nostrum, itaque eos dolorem! Obcaecati consectetur at cupiditate deserunt.</p>
+  </div>;
+}
